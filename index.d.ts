227ec7d18ee45df1b9548f145de2be475c784343
Igor Strebezhev <xamgore@users.noreply.github.com>
2020-05-26 20:32:59 +0300

    Add rawListeners field
    Added in: v9.4.0: https://nodejs.org/api/events.html#events_emitter_rawlisteners_eventname
    
    Typescript globals.d.ts definition:
      rawListeners(event: string | symbol): Function[];

--- index.d.ts
+++ index.d.ts
@@ -33,6 +33,7 @@ interface TypedEventEmitter<Events> {
 
   emit<E extends keyof Events> (event: E, ...args: Arguments<Events[E]>): boolean
   eventNames (): (keyof Events | string | symbol)[]
+  rawListeners<E extends keyof Events> (event: E): Function[]
   listeners<E extends keyof Events> (event: E): Function[]
   listenerCount<E extends keyof Events> (event: E): number
 
